89621019f497a673f392b69ce736cd63e4e47769
Aymen Sekhri <Aymen.sekhri@live.fr>
2019-05-17 15:14:24 +0200

    Add files via upload

---
+++ K-Means image compression/K-Mean Image Conration/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace K_Mean_Image_Conration
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		Matrix<double> PicMat;
+		const int PicSize = 128; // chnage it to the image size (large sizes may cause slow learning)
+		const int ColorsNumber = 16; //Number of Color Clusters (increase it to get better quality)
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+			KMeans KM = new KMeans(ColorsNumber);
+			Vector<double> Assigns = KM.Train(PicMat, 5);
+			Bitmap pic = new Bitmap(PicSize, PicSize);
+			for (int y = 0; y < PicSize; y++)
+			{
+				for (int x = 0; x < PicSize; x++)
+				{
+					int R = Convert.ToInt32(KM.ClustersLocation[(int)Assigns[x + PicSize * y], 0]);
+					int G = Convert.ToInt32(KM.ClustersLocation[(int)Assigns[x + PicSize * y], 1]);
+					int B = Convert.ToInt32(KM.ClustersLocation[(int)Assigns[x + PicSize * y], 2]);
+					pic.SetPixel(x, y, Color.FromArgb(R, G, B));
+				}
+			}
+			pictureBox1.Image = pic;
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog fd = new OpenFileDialog();
+			if (fd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				Image img;
+				using (var bmpTemp = new Bitmap(fd.FileName))
+				{
+					img = new Bitmap(bmpTemp);
+				}
+				Bitmap pic = new Bitmap(img, PicSize, PicSize);
+				PicMat = Matrix<double>.Build.Dense(PicSize * PicSize, 3);
+
+				Color p;
+				for (int y = 0; y < PicSize; y++)
+				{
+					for (int x = 0; x < PicSize; x++)
+					{
+						p = pic.GetPixel(x, y);
+						PicMat[x + PicSize * y, 0] = p.R;
+						PicMat[x + PicSize * y, 1] = p.G;
+						PicMat[x + PicSize * y, 2] = p.B;
+					}
+				}
+
+				pictureBox1.Image = pic;
+			}
+		}
+	}
+}
---
+++ K-Means image compression/K-Mean Image Conration/KMeans.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+
+namespace K_Mean_Image_Conration
+{
+
+	class KMeans
+	{
+		int NumberOfClusters = 1;
+		public Matrix<double> ClustersLocation;
+		public KMeans(int _NumberOfClusters)
+		{
+			NumberOfClusters = _NumberOfClusters;
+
+		}
+		public Vector<double> Train(Matrix<double> inputs, int iterations)
+		{
+			ClustersLocation = Matrix<double>.Build.Dense(NumberOfClusters, inputs.ColumnCount);
+			RandomizeMatrix(inputs,ref ClustersLocation);
+			Vector<double> InputAssignment = Vector<double>.Build.Dense(inputs.RowCount);
+			for (int iter = 0; iter < iterations; iter++)
+			{
+				Assign(inputs, ref InputAssignment);
+				MoveToTheMean(inputs, InputAssignment);
+			}
+			return InputAssignment;
+		}
+		void Assign(Matrix<double> inputs, ref Vector<double> InputAssignment)
+		{
+			Vector<double> Scores = Vector<double>.Build.Dense(NumberOfClusters);
+			Vector<double> DisVector, DisVector1, DisVector2;
+			for (int i = 0; i < inputs.RowCount; i++)
+			{
+				for (int c = 0; c < NumberOfClusters; c++)
+				{
+					DisVector1 = Vector<double>.Build.DenseOfArray(ClustersLocation.ToRowArrays()[c]);
+					DisVector2 = Vector<double>.Build.DenseOfArray(inputs.ToRowArrays()[i]);
+					DisVector = DisVector1 - DisVector2;
+					Scores[c] = DisVector.DotProduct(DisVector);
+				}
+				InputAssignment[i] = Scores.MinimumIndex();
+				Scores.Clear();
+			}
+
+		}
+		void MoveToTheMean(Matrix<double> inputs, Vector<double> InputAssignment)
+		{
+			Matrix<double> NewLocations = Matrix<double>.Build.Dense(ClustersLocation.RowCount, ClustersLocation.ColumnCount);
+			Vector<double> Scores = Vector<double>.Build.Dense(NumberOfClusters);
+			for (int i = 0; i < inputs.RowCount; i++)
+			{
+				for (int c = 0; c < inputs.ColumnCount; c++)
+				{
+					NewLocations[(int)InputAssignment[i], c] += inputs[i, c];
+				}
+				Scores[(int)InputAssignment[i]]++;
+
+			}
+			for (int c = 0; c < NumberOfClusters; c++)
+			{
+				for (int i = 0; i < ClustersLocation.ColumnCount; i++)
+				{
+					if (Scores[c] == 0)
+					{
+						NewLocations[c, i] = ClustersLocation[c, i];
+					}
+					else
+					{
+						NewLocations[c, i] = NewLocations[c, i] / Scores[c];
+					}
+				}
+			}
+			ClustersLocation = NewLocations;
+		}
+		public void RandomizeMatrix( Matrix<double> Inputs,ref Matrix<double> Mat)
+		{
+			Random ran = new Random();
+			for (int i = 0; i < Mat.RowCount; i++)
+			{
+				for (int c = 0; c < Mat.ColumnCount; c++)
+				{
+					Mat[i, c] = ran.Next(0, 255);
+				}
+			}
+		}
+	}
+}
